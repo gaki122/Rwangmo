897af49e4a403257a510fa987d65e8b173eff5c3
Rinzin Wangmo <08230181.sce@rub.edu.bt>
2025-11-15 12:07:34 +0600

    project

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function handleUserLogin() {
+    // Redirect to user login page
+    window.location.href = 'user-login.html';
+}
+
+function handleAdminLogin() {
+    // Redirect to admin login page
+    window.location.href = 'admin-login.html';
+}
